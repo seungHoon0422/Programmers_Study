864bbcaf73fdc3af1d1399825cc2d7584cb094f2
BaekSeungHoon <psh104404@naver.com>
2022-06-09 03:08:08 +0900

    2021 kakao blind level1

---
+++ src/Kakao_Blind_Recruitment_2021/Solution_신규아이디추천.java
@@ -0,0 +1,29 @@
+package Kakao_Blind_Recruitment_2021;
+
+class Solution_신규아이디추천 {
+    public String solution(String id) {
+        String answer = "";
+
+        id = id.toLowerCase();
+        for(int i=0; i<id.length(); i++){
+            char c = id.charAt(i);
+            if('a'<=c && c<='z') continue;
+            else if('0'<=c && c<='9') continue;
+            else if(c=='-' || c=='_' || c=='.') continue;
+            id = id.replace(c, 'A');
+        }
+        id = id.replaceAll("A", "");
+        while(id.contains(".."))
+            id = id.replace("..", ".");
+        if(id.length() > 0 && id.charAt(0) == '.') id = id.substring(1);
+        if(id.length() > 0 && id.charAt(id.length()-1) == '.') id = id.substring(0, id.length()-1);
+        if(id.length()  == 0 ) id = "a";
+        if(id.length()>=16) id = id.substring(0, 15);
+        if(id.charAt(id.length()-1) == '.') id = id.substring(0, id.length()-1);
+        char lastch = id.charAt(id.length()-1);
+        while(id.length() < 3) {
+            id += lastch;
+        }
+        return id;
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,6 @@
-import java.sql.Array;
-import java.util.*;
-
 class Solution {
-    private int[] info;
-    private int n;
-    private int answer;
-    private ArrayList<Integer>[] child;
-
-    public int solution(int[] in, int[][] e) {
-        answer = 0;
-        n = in.length;
-        info = new int[n];
-        info = in.clone();
-        child = new ArrayList[n];
-        for(int[] edge : e) {
-            int p = edge[0];
-            int ch = edge[1];
-            if(child[p] == null) child[p] = new ArrayList<>();
-            child[p].add(ch);
-        }
-
-
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
-        travel(0, 0,0, list);
-
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
         return answer;
-    }
-
-    private void travel(int index, int sheep, int wolf, List<Integer> nextPos) {
-
-        if(info[index]==0) sheep++;
-        else wolf++;
-
-        if(wolf >= sheep) return;
-        answer = Math.max(answer, sheep);
-
-        ArrayList<Integer> next = new ArrayList<>(nextPos);
-        next.remove(Integer.valueOf(index));
-        if(child[index] != null){
-            next.addAll(child[index]);
-        }
-
-        for(int i : next) {
-            travel(i, sheep, wolf, next);
-        }
     }
 }
